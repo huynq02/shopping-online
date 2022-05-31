0203493c702ce9b06c38ef865afdad13aeca7107
vmnhat201 <105227197+vmnhat201@users.noreply.github.com>
2022-05-31 23:33:27 +0700

    update page product
    update page product

--- shopping-online/shopping-online/Controllers/ProductController.cs
+++ shopping-online/shopping-online/Controllers/ProductController.cs
@@ -11,7 +11,17 @@ namespace shopping_online.Controllers
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+            //var objProduct = obj.Products.Where(n => n.product_id == Id).FirstOrDefault();
+            //var objImg = obj.Image_product.Where(x => x.id == objProduct.image_id).FirstOrDefault();
+            //var listCategory = obj.Categories.ToList();
+            //var listColor = obj.Colors.ToList();
+            //ProductDetail productDetail = new ProductDetail();
+            //productDetail.objProduct = objProduct;
+            //productDetail.objImage = objImg;
+            //productDetail.lstCategory = listCategory;
+            //productDetail.lstColor = listColor;
+
+            //return View(productDetail);
         }
     }
 }
