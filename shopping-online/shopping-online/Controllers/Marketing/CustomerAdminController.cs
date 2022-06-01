dcf038af1c2e14a9d17e42617e5279ef52b50ded
Kieu2001 <105227181+Kieu2001@users.noreply.github.com>
2022-06-01 12:03:12 +0700

    Add CustomerHtml

---
+++ shopping-online/shopping-online/Controllers/Marketing/CustomerAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shopping_online.Controllers.Marketing
+{
+    public class CustomerAdminController : Controller
+    {
+        // GET: CustomerAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
