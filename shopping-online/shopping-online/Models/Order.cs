11dd2d91390325da801d2587209b7666ea3d6daf
phuongnt2811 <phuong282821@gmail.com>
2022-06-01 12:04:57 +0700

    add sale

---
+++ shopping-online/shopping-online/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace shopping_online.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; } 
+    }
+}
