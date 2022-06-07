0e045efe1de0b3094d8431963b651c45b9e99431
huynq02 <quanghuytb111@gmail.com>
2022-06-07 23:14:25 +0700

    update code Account

---
+++ shopping-online/shopping-online/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using shopping_online.Controllers;
+using shopping_online.Models;
+using System.Data.SqlClient;
+
+namespace shopping_online.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString() 
+        {
+            con.ConnectionString = "data source= ADMIN-PC\SQLSERVERHUY; database=Project_SU22_lan3_TuanAnh; intergrated security = SSPI; ";
+        }
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select* from Account where account_name";
+            return View();
+        //    if (Dr.Read());
+        //    {
+        //        return View();
+        //    }
+        //    else 
+        //    {
+        //        return View();
+        //    }
+        //    con.Close();
+        //}
+
+    }
+}
--- shopping-online/shopping-online/Models/Account.cs
+++ shopping-online/shopping-online/Models/Account.cs
@@ -5,7 +5,9 @@ using System.Web;
 
 namespace shopping_online.Models
 {
-    public class _1234
+    public class Account
     {
+        public string account_name { get; set; }
+        public string account_password { get; set; }
     }
 }
