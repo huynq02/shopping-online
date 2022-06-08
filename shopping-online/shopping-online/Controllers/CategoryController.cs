ea83e9b3fd3d19d63133fd8e17cbf975a0dd548b
vmnhat201 <105227197+vmnhat201@users.noreply.github.com>
2022-06-08 23:46:36 +0700

    product by category
    product by category 08-06

---
+++ shopping-online/shopping-online/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shopping_online.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            //var listProductByCategory = objCategory.Products.Where(n => n.category_id == Id).ToList();
+            //var listC = objCategory.Categories.ToList();
+            //var listColor = objCategory.Colors.ToList();
+            //var listImage = objCategory.Image_product.ToList();
+            //ProductByCategory p1 = new ProductByCategory();
+            //p1.listProductByCate = listProductByCategory;
+            //p1.listC = listC;
+            //p1.listColor = listColor;
+            //p1.listI = listImage;
+            //return View(p1);
+        }
+    }
+}
