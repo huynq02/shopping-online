19aa0c6c943cca587c35ba1fc1e17d2540ca342a
phuongnt2811 <phuong282821@gmail.com>
2022-06-17 07:36:44 +0700

    phuong

--- shopping-online/shopping-online/Controllers/OrderController.cs
+++ shopping-online/shopping-online/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 using shopping_online.Models;
 
 
@@ -13,26 +14,47 @@ namespace shopping_online.Controllers
     {
         DBContext db = new DBContext();
         // GET: Sale
-       
-        public ActionResult Orders()
+        public class InstructorIndexData
         {
-           
+            public PagedList.IPagedList<Order> Shipping { get; set; }
+
+        }
+        public ActionResult Orders(string table_search, int? page)
+        {
+            int padeNum = (page ?? 1);
+            int pageSize = 10;
             List<Order> orders = db.Orders.ToList();
-            return View(orders);
+            IQueryable<Order> od = db.Orders;
+            IQueryable<Order_Details> ords = db.Order_Details;
+            IQueryable<Account> accounts = db.Accounts;
+           
+            if (!string.IsNullOrEmpty(table_search))
+            {
+                od = od.Where(x => x.Order_note.Contains(table_search));
+                accounts = accounts.Where(x => x.account_username.Contains(table_search));
+            }
+            var pto = od.OrderBy(x=> x.Order_Date).ToPagedList(padeNum, pageSize);
+            ViewBag.table_search = table_search;
+            return View(pto);
 
         }
         [HttpGet]
-        public ActionResult Detail(int id)
+        public ActionResult Details(int id)
         {
+          
             Order order = db.Orders.FirstOrDefault(x => x.Order_id == id);
-            ViewBag.orderId = order.Order_id;
-            if(order == null)
+            Order_Details orderdetails = db.Order_Details.FirstOrDefault(x => x.Order_id == id);
+           
+            
+            if (order == null&& orderdetails == null)
             {
                 Response.StatusCode = 404;
                 return null;
             }
-            return View(order);
+            return View(orderdetails);
         }
+        [HttpPost]
+   
        public ActionResult Create()
         {
             return View();
