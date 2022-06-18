d59d18294ca8162c3dcf5f3adce97ec18158f31e
huynq02 <103916957+huynq02@users.noreply.github.com>
2022-06-18 18:04:13 +0700

    update

---
+++ shopping-online/shopping-online/Controllers/Account/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shopping_online.Controllers.Login
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            if (Session["account_id"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }   
+    }
+}
