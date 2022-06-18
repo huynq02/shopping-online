e84d3dd7fdbdc906237135f78b60981b2b5f292b
huynq02 <103916957+huynq02@users.noreply.github.com>
2022-06-18 15:21:20 +0700

    update

---
+++ shopping-online/shopping-online/Controllers/Login/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using shopping_online.Models;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+namespace shopping_online.Controllers.Login
+{
+    public class LoginController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source = ADMIN; database=Project_SU22; integrated security=true;";
+        }
+        [HttpPost]
+        public ActionResult verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from Account ";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                //return View(""Error"");
+            }
+        }
+    }
+}
---
+++ shopping-online/shopping-online/Controllers/Login/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using shopping_online.Models;
+
+namespace shopping_online.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
