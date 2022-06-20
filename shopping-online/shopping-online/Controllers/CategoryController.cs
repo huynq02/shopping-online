a85c6f1b30cf9cdaa71440e45f6dd16e816f7b25
vmnhat201 <105227197+vmnhat201@users.noreply.github.com>
2022-06-20 08:30:45 +0700

    category

--- shopping-online/shopping-online/Controllers/CategoryController.cs
+++ shopping-online/shopping-online/Controllers/CategoryController.cs
@@ -11,16 +11,16 @@ namespace shopping_online.Controllers
         // GET: Category
         public ActionResult Index()
         {
-            //var listProductByCategory = objCategory.Products.Where(n => n.category_id == Id).ToList();
-            //var listC = objCategory.Categories.ToList();
-            //var listColor = objCategory.Colors.ToList();
-            //var listImage = objCategory.Image_product.ToList();
-            //ProductByCategory p1 = new ProductByCategory();
-            //p1.listProductByCate = listProductByCategory;
-            //p1.listC = listC;
-            //p1.listColor = listColor;
-            //p1.listI = listImage;
-            //return View(p1);
+            var listProductByCategory = objCategory.Products.Where(n => n.category_id == Id).ToList();
+            var listC = objCategory.Categories.ToList();
+            var listColor = objCategory.Colors.ToList();
+            var listImage = objCategory.Image_product.ToList();
+            ProductByCategory p1 = new ProductByCategory();
+            p1.listProductByCate = listProductByCategory;
+            p1.listC = listC;
+            p1.listColor = listColor;
+            p1.listI = listImage;
+            return View(p1);
         }
     }
 }
