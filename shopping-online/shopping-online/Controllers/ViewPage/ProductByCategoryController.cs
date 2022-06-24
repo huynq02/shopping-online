c63bfefeeac92c2b1cb8256b4365b5f579b6a27d
vmnhat201 <105227197+vmnhat201@users.noreply.github.com>
2022-06-24 10:14:41 +0700

    commit all

---
+++ shopping-online/shopping-online/Controllers/ViewPage/ProductByCategoryController.cs
@@ -0,0 +1,31 @@
+﻿using shopping_online.Context;
+using shopping_online.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shopping_online.Controllers.ViewPage
+{
+    public class ProductByCategoryController : Controller
+    {
+        DBContext objCategory = new DBContext();
+        // GET: Category
+        public ActionResult Index(int Id)
+        {
+            var listProductByCategory = objCategory.products.Where(n => n.category_id == Id).ToList();
+            var listC = objCategory.Categories.ToList();
+            var listColor = objCategory.Colors.ToList();
+            //var listImage = objCategory..ToList();
+            var listP = objCategory.products.ToList();
+            ProductByCategory p1 = new ProductByCategory();
+            p1.listProductByCate = listProductByCategory;
+            p1.listC = listC;
+            p1.listColor = listColor;
+            //p1.listI = listImage;
+            p1.listP = listP;
+            return View(p1);
+        }
+    }
+}
---
+++ shopping-online/shopping-online/Models/ListHome.cs
@@ -0,0 +1,16 @@
+﻿using PagedList;
+using shopping_online.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace shopping_online.Models
+{
+    public class ListHome
+    {
+        public IPagedList<product> listProduct { get; set; }
+        public List<Category> listCategory { get; set; }
+        public List<Color> listColor { get; set; }
+    }
+}
