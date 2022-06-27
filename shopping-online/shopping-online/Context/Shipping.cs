84160a6fd0724ad5c58329a7c476b2676da6c114
phuongnt2811 <phuong282821@gmail.com>
2022-06-28 01:05:44 +0700

    update ship and data Annotations

--- shopping-online/shopping-online/Context/Shipping.cs
+++ shopping-online/shopping-online/Context/Shipping.cs
@@ -11,7 +11,9 @@ namespace shopping_online.Context
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class shipping
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,12 +21,21 @@ namespace shopping_online.Context
         {
             this.Orders = new HashSet<Order>();
         }
-    
+
         public int shipping_id { get; set; }
+        [Required(ErrorMessage = "Name is Required")]
+        [DisplayName("Name")]
         public string shipping_name { get; set; }
+        [Required(ErrorMessage = "Email is Required")]
+        [DisplayName("Email")]
+
+        [RegularExpression(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", ErrorMessage = "Please enter a valid email address")]
         public string shipping_email { get; set; }
+        [DisplayName("Phone Number")]
+        [Required(ErrorMessage = "Phone Number is Required")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Please Enter a valid Phone Number")]
         public string shipping_phone { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Order> Orders { get; set; }
     }
---
+++ shopping-online/shopping-online/Controllers/Sale/shippingController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using shopping_online.Context;
+
+namespace shopping_online.Controllers.Sale
+{
+    public class shippingController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: shipping
+        public ActionResult Index(string table_search, int? page)
+        {
+            int padeNum = (page ?? 1);
+            int pageSize = 10;
+            List<shipping> ship = db.shippings.ToList();
+            IQueryable<shipping> pt = db.shippings;
+
+            if (!string.IsNullOrEmpty(table_search))
+            {
+                pt = pt.Where(x => x.shipping_name.Contains(table_search) || x.shipping_email.Contains(table_search) || x.shipping_phone.Contains(table_search));
+            }
+            var pts = pt.OrderBy(x => x.shipping_id).ToPagedList(padeNum, pageSize);
+            ViewBag.table_search = table_search;
+            return View(pts);
+        }
+
+        // GET: shipping/Details/5
+       
+
+        // GET: shipping/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: shipping/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "shipping_id,shipping_name,shipping_email,shipping_phone")] shipping shipping)
+        {
+            if (ModelState.IsValid)
+            {
+                db.shippings.Add(shipping);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(shipping);
+        }
+
+        // GET: shipping/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            shipping shipping = db.shippings.Find(id);
+            if (shipping == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shipping);
+        }
+
+        // POST: shipping/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "shipping_id,shipping_name,shipping_email,shipping_phone")] shipping shipping)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shipping).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(shipping);
+        }
+
+        // GET: shipping/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            shipping shipping = db.shippings.Find(id);
+            if (shipping == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shipping);
+        }
+
+        // POST: shipping/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            shipping shipping = db.shippings.Find(id);
+            db.shippings.Remove(shipping);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
