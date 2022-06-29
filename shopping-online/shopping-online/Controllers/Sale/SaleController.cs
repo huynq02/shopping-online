e9a62c128e0d51fb9e724fbe23ce91fa2afb0aeb
phuongnt2811 <phuong282821@gmail.com>
2022-06-29 11:49:53 +0700

    update profile

---
+++ shopping-online/shopping-online/Controllers/Sale/SaleController.cs
@@ -0,0 +1,37 @@
+﻿using shopping_online.Context;
+using shopping_online.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shopping_online.Controllers.Sale
+{
+    public class SaleController : Controller
+    {
+        DBContext db = new DBContext();
+        // GET: Sale
+        public ActionResult Index()
+        {
+            var lsProHaveOd = db.Order_Details.Where(x => x.product_id.HasValue).ToList();
+            var numProHaveOd = db.Order_Details.Where(x => x.product_id.HasValue).Count();
+            var lsOrder = db.Orders.ToList();
+            var numOrder = db.Orders.Count();
+            var liscushaveOd = db.Orders.Where(x => x.account_id.HasValue).ToList();
+            var numcushaveOd = db.Orders.Where(x => x.account_id.HasValue).FirstOrDefault();
+            var cus = db.Roles.Where(x => x.Role_name == "Customer").FirstOrDefault();
+            double sum = 0;
+            foreach (var item in lsOrder)
+            {
+                sum += item.Order_total_money;
+            }
+            var ordcre = db.Orders.Where(x => EntityFunctions.TruncateTime(x.Order_Date) == EntityFunctions.TruncateTime(DateTime.Now)).FirstOrDefault();
+            SaleDashboard sale = new SaleDashboard();
+            
+            
+            return View(); 
+        }
+    }
+}
---
+++ shopping-online/shopping-online/Models/SaleDashboard.cs
@@ -0,0 +1,22 @@
+﻿using shopping_online.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace shopping_online.Models
+{
+    public class SaleDashboard
+    {
+        // tinhs tong tien 
+       public double sum { get; set; }
+        // hien thi danh sach customer order trong thang
+        public List<Order> cus_order { get; set; }
+        // hien ti ra tong so oder
+        public  Order Order { get; set; }
+        // danh sach san pham duoc order 
+        public List<Order_Details> prodetail { get; set; }
+        public List<product> products { get; set; }
+        
+    }
+}
